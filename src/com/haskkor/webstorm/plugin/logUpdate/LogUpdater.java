1b5d56f598324bf2c668a870674f18312ed3f0cc
Jérémy Farnault <haskkor@gmail.com>
2019-02-10 19:00:16 +1300

    Update LogUpdater.java
    Function and regex ok. Need to create the charsequence for the replace string

--- src/com/haskkor/webstorm/plugin/logUpdate/LogUpdater.java
+++ src/com/haskkor/webstorm/plugin/logUpdate/LogUpdater.java
@@ -4,15 +4,35 @@ import com.intellij.openapi.editor.Document;
 public class LogUpdater {
 
     private final Document document;
+    private int startIndex;
+    private String regex;
 
-    public LogUpdater(Document document) {
+    public LogUpdater(Document document, int startIndex) {
         this.document = document;
+        this.startIndex = startIndex;
+        // Find the console logs containing a line number
+        this.regex = "console\.log\((\"|\')[^\"\'\s]*\sl\.[0-9]+\s[^\"\']*(\"|\')[^\)]*\);?";
     }
 
-    public void findConsoleLogs() {
-        CharSequence sequence = document.getCharsSequence();
+    // public void updateConsoleLogs() {
         // function with a range finding for the first occurence of console.log(“aaaaaaaa l.0, update it and rescursive with a smaller range. 
         // have to figure out how to change the text
-        
+    // }
+    
+    private void updateConsoleLogs() {
+        TextRange textRange = new TextRange(this.startIndex, this.document.getTextLength());
+        String text = this.document.getText(textRange);
+        Pattern pattern = Pattern.compile(this.regex);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            int offsetStart = matcher.start();
+            int offsetEnd = matcher.end();
+            WriteCommandAction.runWriteCommandAction(project, () ->
+                document.replaceString(offsetStart, offsetEnd, "")
+            );
+        }
     }
 }
+
+// https://github.com/JetBrains/intellij-community/blob/master/platform/core-api/src/com/intellij/openapi/editor/Document.java
+// https://github.com/JetBrains/intellij-community/blob/master/platform/util/src/com/intellij/openapi/util/TextRange.java
